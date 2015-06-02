b4fe6158a1e21986d577559c48d1ad9339945a51
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-02 19:45:56 +0100

    Exercise 3-3

---
+++ 3-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Pi = 3.14159;
+            Pi = 3.1;
+            Console.WriteLine(Pi);
+        }
+    }
+}
